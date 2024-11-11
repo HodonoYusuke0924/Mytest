8faac40451880f7a90946e808e84ca81dce2c96c
HY <136606754+HodonoYusuke0924@users.noreply.github.com>
2024-11-11 12:11:34 +0900

    ネットワークその１
    サーバ、クライアントの確立・接続

---
+++ Garvage_war/Net/client_func.h
@@ -0,0 +1,19 @@
+/*****************************************************************
+ファイル名	: client_func.h
+機能		: クライアントの外部関数の定義
+*****************************************************************/
+
+#ifndef _CLIENT_FUNC_H_
+#define _CLIENT_FUNC_H_
+
+#include"common.h"
+
+/* client_net.c */
+extern int SetUpClient(char* hostName,int *clientID,int *num,char clientName[][MAX_NAME_SIZE]);
+extern void CloseSoc(void);
+extern int RecvIntData(int *intData);
+extern void SendData(void *data,int dataSize);
+extern int SendRecvManager(void);
+
+
+#endif
---
+++ Garvage_war/Net/common.h
@@ -0,0 +1,16 @@
+/*****************************************************************
+ファイル名	: common.h
+機能		: サーバーとクライアントで使用する定数の宣言を行う
+*****************************************************************/
+
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/types.h>
+#include<assert.h>
+#include<math.h>
+
+#endif
